1501a3ecafca9046d28389dd166dcf787bbb170f
LeeJaeBin <2jaebin@gmail.com>
2026-05-05 21:56:02 +0900

    chore : 기타 설정

--- app/graphs/nodes/retrieve_node.py
+++ app/graphs/nodes/retrieve_node.py
@@ -2,7 +2,6 @@ from langsmith import traceable
 from app.services.vector_store_service import get_vector_store_from_chroma
 from langchain_core.documents import Document
 
-
 @traceable(name="retrieve_from_vector_store")
 def retrieve_from_vector_store(query: str) -> list[Document]:
     vector_store = get_vector_store_from_chroma()
@@ -10,4 +9,7 @@ def retrieve_from_vector_store(query: str) -> list[Document]:
 
 if __name__ == "__main__":
     # python -m app.services.vector_store_service
-    print(retrieve_from_vector_store("객실내 금지되는 행위"))
+    print(retrieve_from_vector_store("우리 아이가 초등학생인데 헬스장을 갈 수 있나요?"))
+
+
+
--- app/services/vector_store_service.py
+++ app/services/vector_store_service.py
@@ -1,11 +1,7 @@
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from app.config.config import EMBEDING_MODEL, CHROMA_DB_PATH, CHROMA_COLLECTION_NAME
-from langchain_core.documents import Document
-from langsmith import traceable
 
-
-@traceable(name="get_vector_store_from_chroma")
 def get_vector_store_from_chroma() -> Chroma:
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDING_MODEL)
     return Chroma(
